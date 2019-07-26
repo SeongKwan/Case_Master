1663fa5a8fd0a6701784cdf05a52b7686e0d002e
Seong Kwan Roh <seongkwan.roh@gmail.com>
2019-07-26 19:29:54 +0900

    Agent - API 등록

--- src/util/agent.js
+++ src/util/agent.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { computed } from 'mobx';
-import commonStore from '../stores/commonStore';
+// import commonStore from '../stores/commonStore';
+import authStore from '../stores/authStore';
 
 // import commonStore from '../stores/commonStore';
 // import errorStore from '../stores/errorStore';
 // import errorHelper from './errorHelper';
 // const API_ROOT = `${process.env.REACT_APP_API_ENDPOINT}`;
-const API_ROOT = `http://localhost:5001`;
-// const API_ROOT = `https://cloudoc-api.herokuapp.com`;
+const API_ROOT = `http://3.113.76.136:3000`;
 
 class Agent {
     constructor(baseURL = null) {
@@ -24,19 +24,16 @@ class Agent {
 /* ------------------------ */
     // Login & SignUp
 
-    signup(userInfo) {
-        return this.axios
-            .post('/auth/signup', 
-            {email: userInfo.email, password: userInfo.password, name: userInfo.name})
-            .catch(this._handleError);
-    }
+    // signup(userInfo) {
+    //     return this.axios
+    //         .post('/auth/signup', 
+    //         {email: userInfo.email, password: userInfo.password, name: userInfo.name})
+    //         .catch(this._handleError);
+    // }
 
-    login(userInfo) {
-        let { token, email } = commonStore;
-        if (!token) token = undefined;
-        if (!email) email = undefined;
+    login({email, password}) {
         return this.axios
-                .post('/auth/login', {email: userInfo.email, password: userInfo.password}, { 
+                .post('/auth/login', {email, password}, { 
                     baseURL: API_ROOT,
                     headers: { 
                         
@@ -45,280 +42,85 @@ class Agent {
                 .catch(this._handleError);
     }
 
-    loginByGoogle() {
-        return this.get('/auth/google');
+    logout({email, password}) {
+        return this.axios
+                .post('/auth/logout', {email, password}, { 
+                    baseURL: API_ROOT,
+                    headers: { 
+                        
+                    }
+                })
+                .catch(this._handleError);
     }
 
 
 /* ------------------------ */
     // ClouDoc - Case
     loadCases() {
-        return this.get(`/cases`);
+        return this.get(`/case`);
     }
 
-    loadCase(caseId) {
-        return this.get(`/cases/${caseId}`);
+    loadCase({caseid}) {
+        return this.get(`/case/${caseid}`);
     }
 
-    postCase(newCase) {
-        return this.post(`/cases`, newCase);
-    }
+    // postCase(newCase) {
+    //     return this.post(`/cases`, newCase);
+    // }
 
-    updateCase(caseId, updatedCase) {
-        return this.patch(`/cases/${caseId}`, updatedCase);
-    }
+    // updateCase(caseId, updatedCase) {
+    //     return this.patch(`/cases/${caseId}`, updatedCase);
+    // }
 
-    deleteCase(caseId) {
-        return this.delete(`/cases/${caseId}`);
-    }
-
-    analyzeCondition(referenceData) {
-        return this.post(`/cases/analyzeCondition`, referenceData);
-    }
-
-    analyzeSymptom(referenceData) {
-        return this.post(`/cases/analyzeSymptom`, referenceData);
-    }
-
-    analyzeLab(referenceData) {
-        return this.post(`/cases/analyzeLab`, referenceData);
-    }
-
-    analyzeTreatment(referenceData) {
-        return this.post(`/cases/analyzeTreatment`, referenceData);
-    }
+    // deleteCase(caseId) {
+    //     return this.delete(`/cases/${caseId}`);
+    // }
 
 
-
-
-    deleteRecordFromCase(caseId, newCase) {
-        return this.put(`/case/${caseId}`, newCase);
-    }
-
-    postExcel(file) {
-        let { token, email } = commonStore;
+    // postExcel(file) {
+    //     let { token, email } = commonStore;
         
-        return this.axios
-        .post(`/api/v1/cases/uploadxlsx`, file, { 
-            baseURL: API_ROOT,
-            headers: { 
-                'content-type': 'multipart/form-data',
-                'Authorization': `bearer ${token}`,
-                'email': `${email}`
-            }
-        })
-        .catch(this._handleError);
-    }
+    //     return this.axios
+    //     .post(`/api/v1/cases/uploadxlsx`, file, { 
+    //         baseURL: API_ROOT,
+    //         headers: { 
+    //             'content-type': 'multipart/form-data',
+    //             'Authorization': `bearer ${token}`,
+    //             'email': `${email}`
+    //         }
+    //     })
+    //     .catch(this._handleError);
+    // }
     // End of Cloudoc - Case
 /* ------------------------ */
-
-
-
-
-/* ------------------------ */
-    // Cloudoc - Clinical DB
-    loadClinicaldbs() {
-        return this.get(`/clinicaldb`);
-    }
-
-    // Symptom
-    loadSymptoms() {
-        return this.get(`/symptoms`);
-    }
-    
-    loadSymptom(symptomId) {
-        return this.get(`/symptoms/${symptomId}`);
-    }
-
-    createSymptom(newSymptomData) {
-        return this.post(`/symptoms`, newSymptomData);
-    }
-
-    updateSymptom(symptomId, updateSymptomData) {
-        return this.patch(`/symptoms/${symptomId}`, updateSymptomData);
-    }
-
-    deleteSymptom(symptomId) {
-        return this.delete(`/symptoms/${symptomId}`);
-    }
-
-    // Lab
-    loadLabs() {
-        return this.get(`/labs`);
-    }
-
-    loadLab(labId) {
-        return this.get(`/labs/${labId}`);
-    }
-
-    createLab(newLabData) {
-        return this.post(`/labs`, newLabData);
-    }
-
-    updateLab(labId, updateLabData) {
-        return this.patch(`/labs/${labId}`, updateLabData);
-    }
-
-    deleteLab(labId) {
-        return this.delete(`/labs/${labId}`);
-    }
-
-    // Exam
-    loadExams() {
-        return this.get(`/exams`);
-    }
-
-    loadExam(examId) {
-        return this.get(`/exams/${examId}`);
-    }
-
-    createExam(newExamData) {
-        return this.post(`/exams`, newExamData);
-    }
-
-    updateExam(examId, updateExamData) {
-        return this.patch(`/exams/${examId}`, updateExamData);
-    }
-
-    deleteExam(examId) {
-        return this.delete(`/exams/${examId}`);
-    }
-
-    // Condition
-    loadConditions() {
-        return this.get(`/conditions`);
-    }
-    
-    loadCondition(conditionId) {
-        return this.get(`/conditions/${conditionId}`);
-    }
-
-    createCondition(newConditionData) {
-        return this.post(`/conditions`, newConditionData);
-    }
-
-    updateCondition(conditionId, updateConditionData) {
-        return this.patch(`/conditions/${conditionId}`, updateConditionData);
-    }
-
-    deleteCondition(conditionId) {
-        return this.delete(`/conditions/${conditionId}`);
-    }
-
-    // Drug
-    loadDrugs() {
-        return this.get(`/drugs`);
-    }
-
-    loadDrug(drugId) {
-        return this.get(`/drugs/${drugId}`);
-    }
-
-    createDrug(newDrugData) {
-        return this.post(`/drugs`, newDrugData);
-    }
-
-    updateDrug(drugId, updateDrugData) {
-        return this.patch(`/drugs/${drugId}`, updateDrugData);
-    }
-
-    deleteDrug(drugId) {
-        return this.delete(`/drugs/${drugId}`);
-    }
-
-    // Reference
-    loadReferences() {
-        return this.get(`/references`);
-    }
-    loadReference(referenceId) {
-        return this.get(`/references/${referenceId}`);
-    }
-
-    loadReferenceByLink(referenceId) {
-        return this.get(`/references/link/${referenceId}`);
-    }
-
-    createReference(newReferenceData) {
-        return this.post(`/references`, newReferenceData);
-    }
-
-    updateReference(referenceId, updateReferenceData) {
-        return this.patch(`/references/${referenceId}`, updateReferenceData);
-    }
-
-    deleteReference(referenceId) {
-        return this.delete(`/references/${referenceId}`);
-    }
-    // End of Clinical DB
-/* ------------------------ */
-
-
-
-
-
-    // User and Auth ----------------------------------------------------------------------------
-    // signup(signupInfo) {
-    //     return axios.post(`/${API_ROOT}/users/signup/`, signupInfo).catch(this._handleError);
-    // }
-
-    // login(loginInfo) {
-    //     return axios.post(`/${API_ROOT}/users/login/`, loginInfo).catch(this._handleError);
-    // }
-
-    // logout() {
-    //     return this.delete(`/users/logout/`);
-    // }
-
-    // loadUser() {
-    //     // return this.get(`/users/${commonStore.user_uuid}/`);
-    // }
-
-    // validatePassword(password) {
-    //     return axios.post(`/${API_ROOT}/users/validate_password/`, { password });
-    // }
-
-
-
 
 
 
     /* Base REST API method */
     get(url) {
         return this.axios
-            .get(`/api/v1${url}`, this.requestConfig)
+            .get(`/api${url}`, this.requestConfig)
             .catch(this._handleError);
     }
     put(url, body) {
         return this.axios
-            .put(`/api/v1${url}`, body, this.requestConfig)
+            .put(`/api${url}`, body, this.requestConfig)
             .catch(this._handleError);
     }
     patch(url, body) {
         return this.axios
-            .patch(`/api/v1${url}`, body, this.requestConfig)
+            .patch(`/api${url}`, body, this.requestConfig)
             .catch(this._handleError);
     }
     post(url, body) {
         return this.axios
-            .post(`/api/v1${url}`, body, this.requestConfig)
+            .post(`/api${url}`, body, this.requestConfig)
             .catch(this._handleError);
     }
     delete(url) {
         return this.axios
-            .delete(`/api/v1${url}`, this.requestConfig)
+            .delete(`/api${url}`, this.requestConfig)
             .catch(this._handleError);
-    }
-
-    _getRequestConfig() {
-        let requestConfig = null;
-        // let accessToken = null;
-        if (true) {
-            requestConfig = { 
-                baseURL: API_ROOT,
-                // headers: { 'Authorization': `Bearer ${accessToken}` }
-            };
-        }
-        return requestConfig;
     }
 
     @computed get requestConfig() {
@@ -326,7 +128,7 @@ class Agent {
             baseURL: API_ROOT,
             headers: {} 
         };
-        let { token, email } = commonStore;
+        let { logOn: {email, token} } = authStore;
         requestConfig.headers['user-type'] = "ADMIN";
         
         if (token) { requestConfig.headers['Authorization'] = `bearer ${token}`; }
